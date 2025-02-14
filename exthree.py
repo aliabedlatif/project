8ee206c317cc663a58d6183f87118e92b936781c
DESKTOP-1GLGR54 <ali@gmail.com>
2025-02-14 03:27:02 +0200

    remove int from password input

--- exthree.py
+++ exthree.py
@@ -1,7 +1,7 @@
 username = input("enter the username:")
-password = int(input("enter the password:"))
+password = input("enter the password:")
 
-if username == "admin" and password == 1234:
+if username == "admin" and password == "1234":
     print("access granted")
 else:
     print("access denied")
