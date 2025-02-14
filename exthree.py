98618e5bc833f43871dbb3245fee08a4a30d9046
DESKTOP-1GLGR54 <ali@gmail.com>
2025-02-14 03:25:18 +0200

    add if statement

--- exthree.py
+++ exthree.py
@@ -1,2 +1,7 @@
-username = input("enter the username: ")
-password = input("enter the password")
+username = input("enter the username:")
+password = int(input("enter the password:"))
+
+if username == "admin" and password == 1234:
+    print("access granted")
+else:
+    print("access denied")
